e2b879650dd324cc4032287aeb922579a6b9c13e
Karol Sz <(none)>
2019-04-28 14:38:00 +0200

    prepare and initialize skeleton of app

---
+++ mqttpwd_ui/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import base64
+import jinja2
+import asyncio
+import aiohttp_jinja2
+from aiohttp import web
+from cryptography import fernet
+from aiohttp_session import get_session, session_middleware, setup
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+from settings import NAME, TEMPLATE_DIR, STATIC_DIR, BASE_DIR
+
+@aiohttp_jinja2.template('index.html')
+async def index(request):
+    return {'title': NAME}
+
+@aiohttp_jinja2.template('register.html')
+async def register(request):
+    return {'title': NAME}
+
+async def create_app():
+    app = web.Application()
+    fernet_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+    setup(app, EncryptedCookieStorage(secret_key, max_age=86400))
+    aiohttp_jinja2.setup(
+        app, loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
+        context_processors=[aiohttp_jinja2.request_processor],)
+    app.router.add_route('GET', '/', index, name='index')
+    app.router.add_route('*', '/register', register, name='register')
+    return app
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    app = loop.run_until_complete(create_app())
+    web.run_app(app, host='0.0.0.0', port='8383')
+
---
+++ mqttpwd_ui/settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+NAME = 'mqttpwdUI'
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
